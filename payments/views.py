56d7929ce79ba0923dadf3632894f952c0d739dd
theo24-sys <timothybanjo42@gmail.com>
2026-04-17 18:35:28 +0300

    Fix NameError: settings not defined in payments/views.py

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
